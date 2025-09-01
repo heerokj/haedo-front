c8bc2a93b336beb4abcc2ca22872a001937f1ab0
heerokj <wjdgmlfhr0321@gmail.com>
2025-09-01 17:24:32 +0900

    feat: Home, Login 기본 페이지 라우트 추가

---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,8 @@
+export const Login = () => {
+  return (
+    <div>
+      <section>1</section>
+      <section>2</section>
+    </div>
+  );
+};
---
+++ src/routes/router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import { RoutePaths } from "./RoutePaths";
+import Home from "../pages/home/Home";
+import { Login } from "../pages/auth/Login";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      { path: "", element: <Home /> },
+      { path: RoutePaths.LOGIN, element: <Login /> },
+    ],
+  },
+]);
