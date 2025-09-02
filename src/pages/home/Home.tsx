4683bcb3ebffeb5dacfdadec045833ad7453324f
heerokj <wjdgmlfhr0321@gmail.com>
2025-09-03 00:56:05 +0900

    chore : 이동 테스트를 위한 링크 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <a href="/login" style={{ margin: "20px" }}>
+        로그인 페이지로 이동
+      </a>
+      <a href="/dashboard" style={{ margin: "20px" }}>
+        대시보드 페이지로 이동
+      </a>
+    </div>
+  );
 }
