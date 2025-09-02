6c3346e01a464ee4da0ff9d7fbaf1921a9e781af
heerokj <wjdgmlfhr0321@gmail.com>
2025-09-03 00:33:28 +0900

    Feat : 로그인 폼 module css 로 변환

--- src/features/auth/components/AuthForm.tsx
+++ src/features/auth/components/AuthForm.tsx
@@ -1,86 +1,77 @@
-import { Button, Checkbox, Form, Input, Flex } from "antd";
-import { Image } from "antd";
+import { Button, Checkbox, Form, Input, Flex, Image } from "antd";
 import logo from "../../../assets/image/haedo-logo.png";
 import SocialLoinButtons from "./SocialLoinButtons";
 import EventContent from "./EventContent";
+import styles from "./AuthForm.module.css";
 
 export default function AuthForm() {
   return (
     <Form
       name="login"
       initialValues={{ remember: true }}
-      style={{
-        maxWidth: 400,
-        maxHeight: 670,
-        padding: "16px 40px",
-        borderRadius: "20px",
-        background: "#fff",
-        boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
+      className={styles.form}
     >
-      <div style={{ margin: "40px 0" }}>
+      {/* 로고 */}
+      <div className={styles.logoWrapper}>
         <Image width={159} height={46} src={logo} preview={false} />
       </div>
+
+      {/* 아이디 */}
       <Form.Item
         name="username"
-        rules={[{ required: true, message: "Please input your Username!" }]}
+        rules={[{ required: true, message: "이메일을 입력해주세요" }]}
         style={{ marginBottom: "12px" }}
       >
         <Input
           placeholder="ex) haedo_company@naver.com"
-          style={{ height: "48px", width: "320px" }}
+          className={styles.input}
         />
       </Form.Item>
+
+      {/* 비밀번호 */}
       <Form.Item
         name="password"
-        rules={[{ required: true, message: "Please input your Password!" }]}
+        rules={[{ required: true, message: "비밀번호를 입력해주세요" }]}
         style={{ marginBottom: "6px" }}
       >
         <Input
           type="password"
           placeholder="비밀번호 입력"
-          style={{ height: "48px", width: "320px" }}
+          className={styles.input}
         />
       </Form.Item>
+
+      {/* 아이디 저장 + 회원가입/비밀번호 찾기 */}
       <Form.Item style={{ width: "320px", marginBottom: "20px" }}>
         <Flex justify="space-between" style={{ width: "100%" }}>
-          {/* 왼쪽 */}
           <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox style={{ color: "#999999" }}>아이디 저장</Checkbox>
+            <Checkbox className={styles.idRemember}>아이디 저장</Checkbox>
           </Form.Item>
-
-          {/* 오른쪽 */}
           <Flex gap="8px">
-            <a href="" style={{ color: "#555555" }}>
+            <a href="" className={styles.link}>
               회원가입
             </a>
-            <span style={{ color: "#EBEBEB" }}>|</span>
-            <a href="" style={{ color: "#555555" }}>
+            <span className={styles.separator}>|</span>
+            <a href="" className={styles.link}>
               비밀번호 찾기
             </a>
           </Flex>
         </Flex>
       </Form.Item>
 
+      {/* 로그인 버튼 */}
       <Form.Item>
         <Button
           block
           type="primary"
           htmlType="submit"
-          style={{
-            height: "51px",
-            width: "320px",
-            fontSize: "16px",
-            fontWeight: "700",
-            marginBottom: "15px",
-          }}
+          className={styles.loginButton}
         >
           로그인
         </Button>
       </Form.Item>
+
+      {/* 소셜 로그인 + 이벤트 */}
       <SocialLoinButtons />
       <EventContent />
     </Form>
